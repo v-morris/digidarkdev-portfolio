e285de6f5a6a63259c9d4d60e53ce271f55627b7
V Morris <digidarkdev@gmail.com>
2020-12-29 16:33:18 -0700

    style(footer): updated code formatting of footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link , Container, Typography, makeStyles } from '@material-ui/core';
-import { GitHub, Twitter } from '@material-ui/icons';
+import React from 'react'
+import { Link, Container, Typography, makeStyles } from '@material-ui/core'
+import { GitHub, Twitter } from '@material-ui/icons'
 
 function Copyright() {
   return (
@@ -12,55 +12,55 @@ function Copyright() {
       {new Date().getFullYear()}
       {'.'}
     </Typography>
-  );
+  )
 }
 
 const useStyles = makeStyles((theme) => ({
   footer: {
     padding: theme.spacing(4),
     backgroundColor: theme.palette.grey[200],
-    textAlign: 'center'
+    textAlign: 'center',
   },
   icons: {
-    padding: theme.spacing(4, 1, 0, 2)
-  }
-}));
+    padding: theme.spacing(4, 1, 0, 2),
+  },
+}))
 
 function SocialMedia() {
-  const classes = useStyles();
+  const classes = useStyles()
   return (
     <Typography variant="body2" color="textSecondary">
       <Link
-      color="inherit"
-      href="https://github.com/v-morris"
-      target="_blank"
-      rel="noopener"
+        color="inherit"
+        href="https://github.com/v-morris"
+        target="_blank"
+        rel="noopener"
       >
-        <GitHub className={classes.icons} aria-hidden='true'/>
+        <GitHub className={classes.icons} aria-hidden="true" />
         GitHub
       </Link>
-        <Link
+      <Link
         color="inherit"
         href="https://twitter.com/DigiDarkDev"
         target="_blank"
         rel="noopener"
-        >
-          <Twitter className={classes.icons} aria-hidden='true' />
-          Twitter
-        </Link>
+      >
+        <Twitter className={classes.icons} aria-hidden="true" />
+        Twitter
+      </Link>
     </Typography>
   )
 }
 
 export default function Footer() {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
-      <footer className={classes.footer}>
-        <Container maxWidth="sm">
-          <Copyright />
-          <SocialMedia />
-        </Container>
-      </footer>
-  );
+    <footer className={classes.footer}>
+      <Container maxWidth="sm">
+        <Copyright />
+        <SocialMedia />
+      </Container>
+    </footer>
+  )
 }
